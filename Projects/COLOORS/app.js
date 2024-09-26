c1abc31e01fda8aef812e02bac3ab0a75b3835bb
Abul kalam <abulkalam0@aol.com>
2024-09-26 13:44:32 -0400

    colorSApp: generating hashid via chroma, adjusting hue, brightnes and saturation and updating each values dynamically

--- Projects/COLOORS/app.js
+++ Projects/COLOORS/app.js
@@ -12,10 +12,15 @@ let initialColors;
 
 
 
+//event listener
+
+
+//sdding event lister for sliders to change the color 
+sliders.forEach(slider=>{
+    slider.addEventListener("input", hslControls );
+})
 
 //functions 
-
-
 //generate colors 
 function generateHex(){
 
@@ -70,11 +75,122 @@ function randomColors(){
     //showing the hasID of that rgb as well 
     hexText.innerText = randomColor;
 
+    //check for contrast 
+    checkContrast(randomColor, hexText)
+
+    //initial colorize sliders 
+    const color = chroma(randomColor);
+    // this is gettijng all the sliders input, such as hue,brightnerss, saturataion
+    const sliders = div.querySelectorAll(".sliders input");
+    // console.log(sliders);
+    // selecting all the inputs ansd setting them 
+
+    const hue = sliders[0];
+    // console.log(hue);
+    const brightnerss = sliders[1];
+    const saturation = sliders[2];
+
+    // now lets create a function to chnage a color of the panel according to theg sliders 
+
+    colorizeSliders(color,hue, brightnerss, saturation);
+
+
     })
 
 }
 
-randomColors();
+//check text contrast 
+function checkContrast( color, text){
 
+    // with help of chromajs we'll loop over and check how bright the color of each
+    //tab is, goes up to 9
+// iof its brighter thne .5, then the text needs to be dark but it will be too bright to 
+// see and otherwise white 
+
+//so leyts get the color contrast number 
+const luminance = chroma(color).luminance();
+if (luminance >.5){
+    text.style.color = "black";
+
+}else{
+    text.style.color = "white";
+
+
+}
+
+}
+
+//to control the color of each tab by sliders 
+
+function colorizeSliders(color,hue, brightnerss, saturation){
+
+    // with the help of chroma we can manage colors such as schaning them by sliders according
+    //to our needs, checkout chroma tab for more info
+
+// so here we setting the saturation of each color based on chroma doc 
+// whereas 0 being the  no sat and 1 being the high, seting max nd min in a way
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const midbright = color.set("hsl.l", .5)
+
+    //scalling the sataration 
+    const scaleSat = chroma.scale([noSat, color, fullSat])
+    //setting the background color of that scale as the scale, so we can adjust it accoridngly 
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+
+
+    // brightness 
+    //setting the scale 
+    const scalebright = chroma.scale(["black", midbright, "white" ])
+    brightnerss.style.backgroundImage = `linear-gradient(to right, ${scalebright(0)}, ${scalebright(1)})`;
+
+// hue 
+// hue gets all the color and then we adjust the brigt and sat of hue 
+
+hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+
+
+
+}
+
+// function for even lister to chnageclolrs 
+
+function hslControls(e){
+    // console.log(e);
+
+    //now lets get tghe each sliders using the dataset we set earlier in html
+    // basically we numbered each tab accoridigly such 0,1,2,3,4,etc
+   // basiccaly it tells us which tab we clicked 
+    const index= e.target.getAttribute("data-bright") ||
+     e.target.getAttribute("data-sat") ||
+      e.target.getAttribute("data-hue");
+    // basiccaly it tells us which tab we clicked 
+    // console.log(index);
+
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+    // console.log(sliders);
+
+    //as above, lets get all three 
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+
+    const bgColor = colorDivs[index].querySelector("h2").innerText;
+console.log(bgColor);
+
+let color = chroma(bgColor)
+
+.set("hsl.s", saturation.value)
+.set("hsl.l", brightness.value)
+.set("hsl.h", hue.value);
+colorDivs[index].style.backgroundColor = color;
+
+}
+
+//Calling the functions 
+
+
+randomColors();
 let getHash = generateHex();
 // console.log(getHash)
